a694f196383e63487f8aef94d61d8e3c35389ee8
awned <65061212+ceilingfans@users.noreply.github.com>
2022-02-12 20:59:22 +0800

    test: readers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 mod configuration;
+mod util;
+
+use crate::configuration::reader::{read_assist, read_chars, read_substr_len};
+use configuration::reader::read_players;
 
 fn main() {
-    println!("Hello, world!");
+    let players = read_players().unwrap();
+    let substr_len = read_substr_len().unwrap();
+    let assist = read_assist().unwrap();
+    let chars = read_chars().unwrap();
+    println!(
+        "players: {}\nlen: {}\nassist: {}\nchars: {:?}",
+        players, substr_len, assist, chars
+    )
 }
