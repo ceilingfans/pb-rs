a7eab2610d308792bb4891d80c3beb59c5cdec0d
awned <65061212+ceilingfans@users.noreply.github.com>
2022-02-12 17:10:47 +0800

    feat(formatters): error and prompt formatters

---
+++ src/util/formatter.rs
@@ -0,0 +1,23 @@
+extern crate termion;
+
+use termion::{style, color};
+
+pub fn format_prompt(s: &str) -> String {
+    format!(
+        "{}{}{}{}",
+        style::Bold,
+        color::Fg(color::LightGreen),
+        s,
+        style::Reset,
+    )
+}
+
+pub fn format_error(msg: &str) -> String {
+    format!(
+        "{}{}error{}: {}",
+        style::Bold,
+        color::Fg(color::Red),
+        style::Reset,
+        msg,
+    )
+}
