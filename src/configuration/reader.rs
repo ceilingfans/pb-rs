dc675ce07d0c3f5e0576a5f0097240a3bae960b0
awned <65061212+ceilingfans@users.noreply.github.com>
2022-02-12 19:39:38 +0800

    feat(reader): chars reader

--- src/configuration/reader.rs
+++ src/configuration/reader.rs
@@ -1,12 +1,14 @@
 extern crate termion;
 
+use std::collections::HashSet;
+
 use rustyline::{error::ReadlineError, Editor};
-use termion::{color, style};
+use termion::{clear, color, style};
 
 use crate::util::formatter;
 
 fn read_u8(prompt: &str, typ: &str, max: u8) -> Result<u8, &'static str> {
-    println!("{}", formatter::format_prompt(prompt));
+    println!("{}{}", clear::All, formatter::format_prompt(prompt));
     let mut rl = Editor::<()>::new();
     let ret: u8;
     loop {
@@ -54,7 +56,8 @@ pub fn read_substr_len() -> Result<u8, &'static str> {
 
 pub fn read_assist() -> Result<bool, &'static str> {
     println!(
-        "{}",
+        "{}{}",
+        clear::All,
         formatter::format_prompt("Do you want assistance? (y)es/(n)o")
     );
     let mut rl = Editor::<()>::new();
@@ -72,6 +75,57 @@ pub fn read_assist() -> Result<bool, &'static str> {
                 }
 
                 return Ok(yes.contains(&lower.as_str()));
+            }
+            Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
+                println!("Successfully exited.");
+                std::process::exit(0);
+            }
+            Err(e) => {
+                println!("Unknown Error: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
+pub fn read_chars() -> Result<HashSet<char>, &'static str> {
+    println!(
+        "{}{}",
+        clear::All,
+        formatter::format_prompt("Enter the characters you want to play with, type 'exit' to stop")
+    );
+    let mut ret = HashSet::<char>::new();
+    let mut rl = Editor::<()>::new();
+    let mut warning = false;
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                let lower = line.to_lowercase();
+                if lower == "exit" {
+                    if ret.len() < 3 {
+                        println!(
+                            "{}",
+                            formatter::format_error("cannot exit before at least 2 characters")
+                        );
+                        warning = true;
+                        continue;
+                    }
+                    return Ok(ret);
+                }
+                if line.len() > 1 {
+                    if !warning {
+                        println!("warning: entering more than 1 character at once will add all characters separately");
+                        warning = true;
+                    }
+                }
+                for c in line.chars() {
+                    if ret.contains(&c) {
+                        println!("info: ignoring {} as it has already been added", &c);
+                        continue;
+                    }
+                    ret.insert(c);
+                }
             }
             Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
                 println!("Successfully exited.");
