fe42e882f44c34260460f0f7a46487cde887c4b3
awned <65061212+ceilingfans@users.noreply.github.com>
2022-02-12 18:13:40 +0800

    feat(readers): assist reader

--- src/configuration/reader.rs
+++ src/configuration/reader.rs
@@ -1,14 +1,12 @@
 extern crate termion;
 
-use rustyline::{Editor, error::ReadlineError};
+use rustyline::{error::ReadlineError, Editor};
 use termion::{color, style};
 
 use crate::util::formatter;
 
 fn read_u8(prompt: &str, typ: &str, max: u8) -> Result<u8, &'static str> {
-    println!(
-        formatter::format_prompt(prompt)
-    );
+    println!("{}", formatter::format_prompt(prompt));
     let mut rl = Editor::<()>::new();
     let ret: u8;
     loop {
@@ -17,21 +15,26 @@ fn read_u8(prompt: &str, typ: &str, max: u8) -> Result<u8, &'static str> {
             Ok(line) => {
                 let n = line.parse::<u8>();
                 if let Ok(num) = n {
-                    if n > max {
-                        println!(formatter::format_error(format!("maximum of {} for {}", max, typ).as_str()));
+                    if num > max {
+                        println!(
+                            "{}",
+                            formatter::format_error(
+                                format!("maximum of {} for {}", max, typ).as_str()
+                            )
+                        );
                         continue;
                     }
                     ret = num;
                     break;
                 } else {
-                    println!(formatter::format_error("number entered was invalid"));
+                    println!("{}", formatter::format_error("number entered was invalid"));
                     continue;
                 }
             }
             Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
                 println!("Successfully exited.");
                 std::process::exit(0);
-            },
+            }
             Err(e) => {
                 println!("Unknown Error: {}", e);
                 std::process::exit(1);
@@ -47,4 +50,37 @@ pub fn read_players() -> Result<u8, &'static str> {
 
 pub fn read_substr_len() -> Result<u8, &'static str> {
     read_u8("Enter the substring length", "substring", 10)
+}
+
+pub fn read_assist() -> Result<bool, &'static str> {
+    println!(
+        "{}",
+        formatter::format_prompt("Do you want assistance? (y)es/(n)o")
+    );
+    let mut rl = Editor::<()>::new();
+    let mut choices = vec!["n", "no"];
+    let mut yes = vec!["y", "ye", "yes"];
+    choices.append(&mut yes);
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                let lower = line.to_lowercase();
+                if !choices.contains(&lower.as_str()) {
+                    println!("{}", formatter::format_error("invalid choice"));
+                    continue;
+                }
+
+                return Ok(yes.contains(&lower.as_str()));
+            }
+            Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
+                println!("Successfully exited.");
+                std::process::exit(0);
+            }
+            Err(e) => {
+                println!("Unknown Error: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
 }
