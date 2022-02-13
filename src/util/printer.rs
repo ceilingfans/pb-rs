c7ec8f67d498d0a8c83bf0f997783ddf43282ab1
awned <65061212+ceilingfans@users.noreply.github.com>
2022-02-13 20:32:02 +0800

    feat(printer): loss stack

--- src/util/printer.rs
+++ src/util/printer.rs
@@ -3,6 +3,8 @@ extern crate termion;
 use termion::color::Red;
 use termion::{clear, color, style};
 
+use substring::Substring;
+
 pub fn print_prompt(s: &str) {
     println!(
         "{}{}{}{}",
@@ -25,7 +27,7 @@ pub fn print_error(msg: &str) {
 
 pub fn print_win(player: u8) {
     println!(
-        "{}{}Victory!{} Player {} has won!",
+        "{}{}Yay!{} Player {} has won!",
         style::Bold,
         color::Fg(color::Yellow),
         style::Reset,
@@ -35,7 +37,7 @@ pub fn print_win(player: u8) {
 
 pub fn print_loss(player: u8) {
     println!(
-        "{}{}Game over!{} Player {} has lost D;",
+        "{}{}Oh no!{} Player {} has lost D;",
         style::Bold,
         color::Fg(color::Red),
         style::Reset,
@@ -45,4 +47,29 @@ pub fn print_loss(player: u8) {
 
 pub fn clear_screen() {
     println!("{}", clear::All);
+}
+
+pub fn print_loss_stack(string: &String, substr_len: u8, ch: &char) {
+    let prefix = string.substring(string.len() - (substr_len - 1) as usize, string.len());
+    let matcher = format!("{}{}", prefix, ch);
+    let pos = string.find(&matcher);
+    if let Some(p) = pos {
+        println!("{}", string);
+        println!(
+            "{}{}{}{}{} <-- {}{}{}{}{}{}",
+            style::Bold,
+            color::Fg(color::Red),
+            " ".repeat(p),
+            "^".repeat(matcher.len()),
+            style::Reset,
+            style::Bold,
+            style::Underline,
+            prefix,
+            color::Fg(color::Red),
+            ch,
+            style::Reset
+        );
+    } else {
+        print_error("internal error, could not get position in util::printer::print_loss_stack, please report this at https://github.com/ceilingfans/pb-rs/issues");
+    }
 }
