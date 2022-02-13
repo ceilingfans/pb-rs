1086a77707ed2860a466cc2eb188c7087eae9081
awned <65061212+ceilingfans@users.noreply.github.com>
2022-02-13 21:38:32 +0800

    chore: main file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 mod configuration;
+mod game;
 mod util;
 
-use crate::configuration::reader::{read_assist, read_chars, read_substr_len};
-use configuration::reader::read_players;
+use crate::game::run;
 
 fn main() {
-    let players = read_players().unwrap();
-    let substr_len = read_substr_len().unwrap();
-    let assist = read_assist().unwrap();
-    let chars = read_chars().unwrap();
-    println!(
-        "players: {}\nlen: {}\nassist: {}\nchars: {:?}",
-        players, substr_len, assist, chars
-    )
+    run::Game::new().run();
 }
